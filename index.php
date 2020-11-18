7c311b39a9e9878ed05553b616fd5463383d9c9e
Don Juji <rwkregime@gmail.com>
2020-11-18 01:39:14 -0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<!-- HTML Codes by Quackit.com -->
+<title>
+</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {background-color:#000000;background-repeat:no-repeat;background-position:top left;background-attachment:fixed;}
+h5{text-align:center;font-family:Helvetica, sans-serif;color:#00ff2a;background-color:#000000;}
+p {text-align:center;font-family:Georgia, serif;font-size:22px;font-style:normal;font-weight:normal;color:#38fe2a;background-color:#000000;}
+</style>
+</head>
+<body>
+<h5>Yeah i dunno its just.....</h5>
+<p>...welll....its just it kinda sketches me out how you guys are like....specifically nostalgic about the axis...I was thinking..."Maybe i shouldn't deface this, maybe they are just history buffs right???"...thing is...why not just be into the history of the world wars in general? Why so focused on the fascists and what awards they got? I could be wrong here but I dunno....i thought about it and...im gonna guess this forum is mostly populated by racists/facists/nationalists. Don't get me wrong I didnt vote for biden or anything but i didnt vote for trump either if you know what i mean. Anyways the axis got pwn't lul, get over it. Find a penis to suck, Hitler is fresh out of penis juice. OH! And keep your forum forum software updated if you dont want to get embarrassed again Mr Admin. </p>
+<p></p>
+<p>P.S I didnt fuck anything up you just need to restore the index page.</p>
+<p></p>
+<p>-DJ ~2020 *Digital Gangster 4 L* </p>
+<p></p>
+<p>BLACK LIVES MATTER</p>
+<br>
+<center><img src="https://digitalgangster.com/6um/img/dgpixel-6um.gif"></center>
+</body>
+</html>
