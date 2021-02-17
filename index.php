e5544135e26463df90cb644afe6ff7ad78e5cc50
Don Juji <rwkregime@gmail.com>
2021-02-16 17:09:17 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,27 +1,50 @@
-<!DOCTYPE html>
 <html>
 <head>
-<!-- HTML Codes by Quackit.com -->
-<title>
-</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<style>
-body {background-color:#000000;background-repeat:no-repeat;background-position:top left;background-attachment:fixed;}
-h5{text-align:center;font-family:Helvetica, sans-serif;color:#00ff2a;background-color:#000000;}
-p {text-align:center;font-family:Georgia, serif;font-size:22px;font-style:normal;font-weight:normal;color:#38fe2a;background-color:#000000;}
-</style>
+	<title>LINGSCARSDOTCOMOWNSYOU</title>
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css"
+  />
 </head>
-<body>
-  <center>
-<h5>Would You Give Your Life For Ling?</h5>
-<p>Once you truly give yourself to ling, all your fears and worries will disipate!</p>
-<p></p>
-<p><img src="https://images.lingscars.com/images/index/footer-speech-bubble.png"></p>
-<p></p>
-<p> ~ Hacked By THE|ONE OF LINGS LITTLE HACKER GANG 2021 ~</p>
-<p></p>
-<p>Greets To Illsec</p>
-<br>
-<img src="https://digitalgangster.com/6um/img/dgpixel-6um.gif"></center>
+<body bgcolor="black" text="#00ff00">
+	<center>
+	
+	<img src="https://images.lingscars.com/images/index/lingscars-logo.png">
+	<div>
+	<MARQUEE><H1>WOW YOU STUPID FUCKIN DUMB IDIOTS LINGSCARS.COM JUST PUT A SPANKING ON UR SECURITY!!!</H1></MARQUEE>
+<MARQUEE><img src="https://images.lingscars.com/images/index/ling-mad-head.png"><img src="https://images.lingscars.com/images/index/ling-mad-head.png"><img src="https://images.lingscars.com/images/index/ling-mad-head.png"><img src="https://images.lingscars.com/images/index/ling-mad-head.png"><img src="https://images.lingscars.com/images/index/ling-mad-head.png"><img src="https://images.lingscars.com/images/index/ling-mad-head.png"><img src="https://images.lingscars.com/images/index/ling-mad-head.png"><img src="https://images.lingscars.com/images/index/ling-mad-head.png"><img src="https://images.lingscars.com/images/index/ling-mad-head.png"><img src="https://images.lingscars.com/images/index/ling-mad-head.png"><img src="https://images.lingscars.com/images/index/ling-mad-head.png"><img src="https://images.lingscars.com/images/index/ling-mad-head.png"><img src="https://images.lingscars.com/images/index/ling-mad-head.png"><img src="https://images.lingscars.com/images/index/ling-mad-head.png"><img src="https://images.lingscars.com/images/index/ling-mad-head.png"></MARQUEE>
+<MARQUEE><img src="https://images.lingscars.com/images/index/ling-oh-no.png"><img src="https://images.lingscars.com/images/index/ling-oh-no.png"><img src="https://images.lingscars.com/images/index/ling-oh-no.png"><img src="https://images.lingscars.com/images/index/ling-oh-no.png"><img src="https://images.lingscars.com/images/index/ling-oh-no.png"><img src="https://images.lingscars.com/images/index/ling-oh-no.png"><img src="https://images.lingscars.com/images/index/ling-oh-no.png"><img src="https://images.lingscars.com/images/index/ling-oh-no.png"><img src="https://images.lingscars.com/images/index/ling-oh-no.png"><img src="https://images.lingscars.com/images/index/ling-oh-no.png"><img src="https://images.lingscars.com/images/index/ling-oh-no.png"><img src="https://images.lingscars.com/images/index/ling-oh-no.png"><img src="https://images.lingscars.com/images/index/ling-oh-no.png"><img src="https://images.lingscars.com/images/index/ling-oh-no.png"><img src="https://images.lingscars.com/images/index/ling-oh-no.png"><img src="https://images.lingscars.com/images/index/ling-oh-no.png"><img src="https://images.lingscars.com/images/index/ling-oh-no.png"><img src="https://images.lingscars.com/images/index/ling-oh-no.png"><img src="https://images.lingscars.com/images/index/ling-oh-no.png">s<img src="https://images.lingscars.com/images/index/ling-oh-no.png"><img src="https://images.lingscars.com/images/index/ling-oh-no.png"><img src="https://images.lingscars.com/images/index/ling-oh-no.png"><img src="https://images.lingscars.com/images/index/ling-oh-no.png"><img src="https://images.lingscars.com/images/index/ling-oh-no.png"><img src="https://images.lingscars.com/images/index/ling-oh-no.png"><img src="https://images.lingscars.com/images/index/ling-oh-no.png"></MARQUEE>
+
+<pre>
+
+    <div class="animate__animated animate__bounce animate__repeat-3">
+	<h1 class="animate__animated animate__bounce">    
+	      _______________________________________ 
+	     |                                       |
+	     | lingscarsdotcom:~$ double yu es oh    |
+	     |                   dot pee ache pee    |
+	     |                                       |
+	     |      i mezzed up ur stinky bawx       |
+	     |         u hunt street bats lul        |
+	     |                                       |
+	     |    how butt frustr8 1 owt ov tin?     |
+	     |_______________________________________|
+	     |_______________________________________|
+  
+  </h1>
+</div>
+<h1>IM LING! YOU CAN TRUST ME!</h1>
+
+</pre>
+
+<img src="https://images.lingscars.com/images/index/ling.png"><img src="https://images.lingscars.com/images/index/ling.png"><img src="https://images.lingscars.com/images/index/ling.png">
+
+<div>
+<h1>WOW LUL U SUR DYD A GUD JAWB OV SEKYOURETTY!!!! JK SKYE!!!! U GOT OWNED NUB, HEH. </h1>
+
+~ Greetz 2 ~
+
+DigitalGangster | Computer Tricksters | SHIMMERY HUNTERS HACKING CLUB | VINKY TROYA
+</center>
 </body>
 </html>
